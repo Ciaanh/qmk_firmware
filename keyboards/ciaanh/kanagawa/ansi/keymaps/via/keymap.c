539f498fdf2b9a88ea78dda9d09d0f855b29cd8f
ciaanh <nicolas.fabre.21@outlook.fr>
2024-08-29 19:57:21 +0200

    Add caps word

--- keyboards/ciaanh/kanagawa/ansi/keymaps/via/keymap.c
+++ keyboards/ciaanh/kanagawa/ansi/keymaps/via/keymap.c
@@ -15,6 +15,37 @@
  */ 
 #include QMK_KEYBOARD_H
 
+bool is_caps_lock_on(void) {
+    led_t led_state = host_keyboard_led_state();
+    return led_state.caps_lock;
+}
+
+enum {
+    TD_CAPS,
+};
+
+void dance_caps(tap_dance_state_t *state, void *user_data) {
+    if (state->count == 1) {
+        if(is_caps_lock_on())
+        {
+            register_code(KC_CAPS);
+        }
+        caps_word_on();
+        reset_tap_dance(state);
+    }
+    if (state->count >= 2) {
+        if(is_caps_word_on()){
+            caps_word_off();
+        }
+        register_code(KC_CAPS);
+        reset_tap_dance(state);
+    }
+}
+
+tap_dance_action_t tap_dance_actions[] = {
+    [TD_CAPS] = ACTION_TAP_DANCE_FN(dance_caps),
+};
+
 enum layers { 
     _BASE,
     _LOWER, 
@@ -63,7 +94,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       
          KC_GRV,    KC_1,       KC_2,       KC_3,       KC_4,       KC_5,    KC_6,    KC_7,    KC_8,      KC_9,    KC_0, KC_MINS,  KC_EQL, KC_BSPC,   KC_DEL,
          KC_TAB,    KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,    KC_Y,    KC_U,    KC_I,      KC_O,    KC_P, KC_LBRC, KC_RBRC, KC_BSLS,  KC_HOME,
-        KC_CAPS,    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,    KC_H,    KC_J,    KC_K,      KC_L, KC_SCLN, KC_QUOT,  KC_ENT,            KC_END,
+    TD(TD_CAPS),    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,    KC_H,    KC_J,    KC_K,      KC_L, KC_SCLN, KC_QUOT,  KC_ENT,            KC_END,
         KC_LSFT, XXXXXXX,       KC_Z,       KC_X,       KC_C,       KC_V,    KC_B,    KC_N,    KC_M,   KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,    KC_UP,
         KC_LCTL, KC_LGUI,    KC_LALT,  TG(_LOWER),        KC_SPC,        KC_SPC,            KC_RALT,TG(_RAISE), KC_RCTL,           KC_LEFT, KC_DOWN, KC_RGHT
     ),
